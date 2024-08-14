f65b59e57faa1de9e0c6057f472fa2e30f7d34ff
MissCISSE <kadidja.cisse@etudiant.univ-rennes.fr>
2024-08-14 08:52:18 +0100

    v1.3 m

--- src/Main.java
+++ src/Main.java
@@ -186,7 +186,7 @@ public class Main {
                                         }
                                         break;
                                     case 4:
-                                        // Lister les élèvesss
+                                         // Lister les élèvesss
                                         System.out.println("Liste des élèves :");
                                         for (Eleve e : listeEleves) {
                                             System.out.println(e);
